7adc800c17ce8cc6819a0d82f191ec3538b3f934
Vitaliy Lyashchenko <69591510+Joroboro253@users.noreply.github.com>
2023-12-21 14:01:51 +0200

    Upgraded db

--- internal/assets/migrations/001_initial.sql
+++ internal/assets/migrations/001_initial.sql
@@ -1,29 +1,13 @@
 -- +migrate Up
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    name TEXT,
-    description TEXT
-);
 
-CREATE TABLE Reviews (
+CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
     product_id INT NOT NULL,
     user_id INT NOT NULL,
-    content TEXT NOT NULL,
+    content TEXT NOT NULL CHECK (length(content) <= 255),
+    rating DECIMAL(2, 1) CHECK (rating >= 1.0 AND rating <= 5.0),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (product_id) REFERENCES products(id)
 );
-
-CREATE TABLE rating (
-    id SERIAL PRIMARY KEY,
-    review_id INT NOT NULL,
-    rating INT CHECK (rating >= 1 AND rating <= 5),
-    FOREIGN KEY (review_id) REFERENCES reviews(id)
-);
-
-
-
-INSERT INTO products (name, description) VALUES ('Тестовый продукт 1', 'Описание тестового продукта 1'), ('Тестовый продукт 2', 'Описание тестового продукта 2')
 
 -- +migrate Down
